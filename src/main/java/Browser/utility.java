e9adee996b8158a8a39f32fb210c2047cb76ae6b
AakashNandi <aakashnandi3@gmail.com>
2026-01-14 06:19:15 +0000

    updated

--- src/main/java/Browser/utility.java
+++ src/main/java/Browser/utility.java
@@ -7,7 +7,7 @@ public class utility {
     public static String properties(String data) throws Exception{  //browser
         String propertiesData = null;
         try{
-            String filePath = System.getProperty("user.dir")+"\\src\\main\\resources\\configure.properties";
+            String filePath = System.getProperty("user.dir")+"//src//main//resources//configure.properties";
             FileReader reader = new FileReader(filePath);
             Properties p = new Properties();
             p.load(reader);
@@ -17,4 +17,11 @@ public class utility {
         }
         return propertiesData;
     }
+
+    public static void main(String[] args) throws Exception{
+        System.out.println("browser");   //from configuration.properties
+        System.out.println("url");
+    }
 }
+
+
